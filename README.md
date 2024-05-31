d2f5781db84721749e4a461f39a95cfb292d5e03
Swanand58 <swanandkhonde1999@gmail.com>
2024-05-31 11:12:20 +0530

    Adding 4 programs

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # LeetCode75Programs
+
+This repository contains solutions to LeetCode75 problems.
+
+## Problems
+
+### [String Compression](https://leetcode.com/problems/string-compression/)
+
+### [Move Zeroes](https://leetcode.com/problems/move-zeroes/)
+
+### [Increasing Triplet Subsequence](https://leetcode.com/problems/increasing-triplet-subsequence/)
+
+### [Container With Most Water](https://leetcode.com/problems/container-with-most-water/)
--- increasingTripletSubSequence.py
+++ increasingTripletSubSequence.py
@@ -1,10 +1,33 @@
+# Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,2,3,4,5]
+# Output: true
+# Explanation: Any triplet where i < j < k is valid.
+# Example 2:
+
+# Input: nums = [5,4,3,2,1]
+# Output: false
+# Explanation: No triplet exists.
+# Example 3:
+
+# Input: nums = [2,1,5,0,4,6]
+# Output: true
+# Explanation: The triplet (3, 4, 5) is valid because nums[3] == 0 < nums[4] == 4 < nums[5] == 6.
+
+
+
 import sys
 import math
+from typing import List
 
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
         if(nums == None and len(nums) < 3):
-            return false
+            return False
         
         min1 = 2**31 
         min2 = 2**31
